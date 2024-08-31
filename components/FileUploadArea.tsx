52c85c1b9c9bfa82d8dbfe8c4ec1565f38073e6d
Ishimwe Gentil <igentil295@gmail.com>
2024-08-31 11:21:14 -0500

    Fileupload changes

--- components/FileUploadArea.tsx
+++ components/FileUploadArea.tsx
@@ -2,28 +2,21 @@ import React from 'react';
 import { useDropzone } from 'react-dropzone';
 
 const FileUploadArea = () => {
-  const onDrop = (acceptedFiles) => {
+  const onDrop = (acceptedFiles: File[]) => {
     // Handle file upload here
     console.log(acceptedFiles);
   };
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
-    <div 
-      {...getRootProps()} 
-      className="border-2 border-dashed border-gray-600  rounded-lg p-12 text-center cursor-pointer flex items-center justify-center h-64"
-    >
+    <div className="border-2 border-dashed border-gray-600  rounded-lg p-12 text-center cursor-pointer flex items-center justify-center h-64"
+     {...getRootProps()}>
       <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the file here ...</p>
-      ) : (
-        <p>Click to add or drop file</p>
-      )}
+      <p>Drag 'n' drop some files here, or click to select files</p>
     </div>
   );
 };
 
 export default FileUploadArea;
 
-const acceptedFIles = []

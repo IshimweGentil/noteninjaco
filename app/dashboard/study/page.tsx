e5184d7e94f329f9493a99e6de4acba3cb7bb132
Ishimwe Gentil <igentil295@gmail.com>
2024-08-31 11:16:13 -0500

    study changes

--- app/dashboard/study/page.tsx
+++ app/dashboard/study/page.tsx
@@ -14,12 +14,12 @@ const StudyPage = () => {
   return (
     <>
       {/* eslint-disable-next-line react/no-unescaped-entities */}
-      <span className="font-bold text-2xl">Let&apos;s Study</span>
+      <span className="font-bold text-2xl">Let's Study</span>
       <div className="border-dashed border-zinc-500 w-full h-12"></div>
       <div className="border-dashed border-zinc-500 w-full h-64"></div>
       
       <StudyContent />
-    </>
+    </> 
   );
 };
 

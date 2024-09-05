643efb71a51c3e9c8dcee76149d6ee5bfb6cee75
Ishimwe Gentil <igentil295@gmail.com>
2024-09-05 11:30:39 -0500

    cookin

--- app/api/pdf-parser/route.ts
+++ app/api/pdf-parser/route.ts
@@ -1,39 +1,25 @@
 import { NextResponse } from 'next/server';
 import pdfParse from 'pdf-parse';
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+export const dynamic = 'force-dynamic';
 
 export async function POST(req: Request) {
   console.log("REQUEST RECEIVED!");
 
-  const streamToBuffer = async (stream: ReadableStream<Uint8Array>) => {
-    const reader = stream.getReader();
-    const chunks: Uint8Array[] = [];
-    let result;
-
-    while (!(result = await reader.read()).done) {
-      chunks.push(result.value);
-    }
-
-    return Buffer.concat(chunks);
-  };
-
   try {
     const formData = await req.formData();
-    console.log("Form data received, number of entries:", formData.entries().length);
+    const entriesCount = Array.from(formData.entries()).length;
+    console.log("Form data received, number of entries:", entriesCount);
 
-    const filePromises: any[] = [];
+    const filePromises: Promise<string>[] = [];
 
     formData.forEach((value, key) => {
       if (value instanceof File) {
         console.log(`Processing file: ${value.name}`);
         const promise = (async () => {
           try {
-            const buffer = await streamToBuffer(value.stream());
+            const arrayBuffer = await value.arrayBuffer();
+            const buffer = Buffer.from(arrayBuffer);
             console.log(`Buffer created for ${value.name}, size: ${buffer.length} bytes`);
             
             const data = await pdfParse(buffer, {
@@ -49,13 +35,13 @@ export async function POST(req: Request) {
         })();
         filePromises.push(promise);
       }
-    })
+    });
 
     const text = await Promise.all(filePromises);
     console.log(`Number of processed texts: ${text.length}`);
 
     const result = text.filter(txt => txt.length > 0).join('\n');
-    console.log(`Number of non-empty texts: ${result.length}`);
+    console.log(`Total length of non-empty texts: ${result.length}`);
 
     return NextResponse.json({ text: result });
   } catch (error) {

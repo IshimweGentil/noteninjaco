80f396503fc99da9dea4dbadbe8451d5e44fbb73
kylehuang041 <kylehuang041@gmail.com>
2024-09-05 19:26:03 -0700

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import NavBar from "../components/NavBar";
 import NavBarMobile from "../components/NavBarMobile";
+import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from "next/script"
 
 // export const metadata: Metadata = {
 //   title: "Note Ninja",
@@ -20,6 +22,18 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-3NLRJP26R0"></Script>
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-3NLRJP26R0');
+            `}
+          </Script>
+        </head>
         <body className="bg-slate-950 text-white min-h-screen flex flex-col">
           <div className="flex">
             <main className="flex-1">

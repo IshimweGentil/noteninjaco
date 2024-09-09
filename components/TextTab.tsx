3a00f31a209dbe2df9bed3a03e281c86b20a4b8e
Ishimwe Gentil <igentil295@gmail.com>
2024-09-09 17:32:08 -0500

    mobile responsiveness, landing page buttons

--- components/TextTab.tsx
+++ components/TextTab.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import FileUploadArea from "./FileUploadArea";
 import AudioTab from "./AudioTab";
 
 interface TextTabProps {
@@ -11,15 +10,17 @@ interface TextTabProps {
 const TextTab: React.FC<TextTabProps> = ({ text, setText, AudioTab }) => {
   return (
     <div className="space-y-4 mb-2 relative">
-      <div className="relative max-w-[1200px]">
+      <div className="relative w-full max-w-[1200px]">
         <textarea
           onChange={(e) => setText(e.target.value)}
           placeholder="Enter your text here..."
-          className="w-full h-64 p-2 border-2 border-gray-600 rounded-md bg-transparent"
+          className="w-full h-64 p-2 pb-12 border-2 border-gray-600 rounded-md bg-transparent resize-y"
           rows={15}
           value={text}
         />
-        <AudioTab className="absolute bottom-4 right-4" text={text} setText={setText} />
+        <div className="absolute bottom-2 right-2">
+          <AudioTab text={text} setText={setText} />
+        </div>
       </div>
     </div>
   );
